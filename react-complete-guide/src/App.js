d546101ce210a3f1e21e8b9dac1bc9e89cca80bd
Parastou63 <35154557+Parastou63@users.noreply.github.com>
2020-08-10 21:52:42 -0700

    React-Learning: Person Function Component, App state, onClick
    Person Function Component: Uses 'name' and 'age' inputs
    App State: Handles the state of the people 'array'.
    onClick: switchNameHandler is click event called if the user clicks the button.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-   render() {
+  state = {
+    people: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    //DON'T DO THID: this.state.people[0].name = 'Maximilian';
+this.setState({
+  people: [
+    { name: 'Maximilian', age: 28 },
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 27 }
+  ]
+})
+  }
+  render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App.🎈</h1>
         <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} >My Hobbies: Racing</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+
       </div>
     );
-  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React App!'));
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React App!'));
   }
 }
 
