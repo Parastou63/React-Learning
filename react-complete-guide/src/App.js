f4b1b47cc674249d7ae531f34720a8a3104dec5b
Parastou63 <35154557+Parastou63@users.noreply.github.com>
2020-08-11 23:58:14 -0700

    React-Learning: Click and Changed Event Binding
    Person component now has onClick and onChange. These events are set through the props.click and props.changed.
    
    App component passes the event handlers to the Person component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,26 +12,54 @@ class App extends Component {
     ],
     otherState: 'some other value'
   }
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked!');
     //DON'T DO THID: this.state.people[0].name = 'Maximilian';
-this.setState({
-  people: [
-    { name: 'Maximilian', age: 28 },
-    { name: 'Manu', age: 29 },
-    { name: 'Stephanie', age: 27 }
-  ]
-})
+    this.setState({
+      people: [
+        { name: newName, age: Math.floor(Math.random() * 30) },
+        { name: 'Manu', age: Math.floor(Math.random() * 30) },
+        { name: 'Stephanie', age: Math.floor(Math.random() * 30) }
+      ]
+    })
+  }
+  nameChangeHandler = (event) => {
+    this.setState({
+      people: [
+        { name: 'Max', age: Math.floor(Math.random() * 30) },
+        { name: event.target.value, age: Math.floor(Math.random() * 30) },
+        { name: 'Stephanie', age: Math.floor(Math.random() * 30) }
+      ]
+    })
   }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App.🎈</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <Person name={this.state.people[1].name} age={this.state.people[1].age} >My Hobbies: Racing</Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+        <button style={style}
+          onClick={() => { this.switchNameHandler('Maximilian!!') }}>Switch Name</button>
+        <Person
+          name={this.state.people[0].name}
+          age={this.state.people[0].age} />
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangeHandler}
+        >My Hobbies: Racing</Person>
+
+        <Person
+          name={this.state.people[2].name}
+          age={this.state.people[2].age} />
 
       </div>
     );
