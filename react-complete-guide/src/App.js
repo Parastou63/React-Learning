5acb37f7fbaaaf9e92c988ad7be5965f0f1b00db
Parastou63 <35154557+Parastou63@users.noreply.github.com>
2020-08-17 20:43:05 -0700

    React-Learning: Toggle Component onClick
    Person Components's display is now toggled based on the this.state.showPeople value.
    render() is called everytime the state changes. We use this to create a people variable and assign it to null. Next based on this.state.showPeople is true, we assign the Person components to 'people'.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
     useroutputs: [
       { userName: 'Matthew' },
       { userName: 'Parastou' }
-    ]
+    ],
+    showPeople: false
   }
 
   switchNameHandler = (newName) => {
@@ -43,40 +44,60 @@ class App extends Component {
   userNameChangeHandler = (event) => {
     this.setState({
       useroutputs: [
-        { userName: event.target.value},
-        { userName: 'Parastou'}
+        { userName: event.target.value },
+        { userName: 'Parastou' }
       ]
     })
 
   }
+
+  togglePeopleHandler = () => {
+    const doesShow = this.state.showPeople;
+
+    this.setState({
+      showPeople: !doesShow
+    })
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+      margin: '8px'
+    };
 
+    let people = null;
+    
+    if (this.state.showPeople) {
+      people = (
+        <div>
+          <Person
+            name={this.state.people[0].name}
+            age={this.state.people[0].age} />
+          <Person
+            name={this.state.people[1].name}
+            age={this.state.people[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangeHandler}
+          >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.people[2].name}
+            age={this.state.people[2].age} />
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I am a React App.🎈</h1>
         <p>This is really working!</p>
         <button style={style}
           onClick={() => { this.switchNameHandler('Maximilian!!') }}>Switch Name</button>
-        <Person
-          name={this.state.people[0].name}
-          age={this.state.people[0].age} />
-        <Person
-          name={this.state.people[1].name}
-          age={this.state.people[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangeHandler}
-        >My Hobbies: Racing</Person>
-
-        <Person
-          name={this.state.people[2].name}
-          age={this.state.people[2].age} />
+        <button style={style}
+          onClick={this.togglePeopleHandler}>Toggle People</button>
+        {people}
         <UserInput
           userName={this.state.useroutputs[0].userName}
           changed={this.userNameChangeHandler}
--- react-complete-guide/src/BaseSyntaxAssignment/UserInput/UserInput.js
+++ react-complete-guide/src/BaseSyntaxAssignment/UserInput/UserInput.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './UserInput.css';
 
 const userInput = (props) => {
-const style = {
-    border: '2px solid red'
-};
+    const style = {
+        border: '2px solid red',
+        display: 'block',
+        margin: '0 auto'
+    };
     return (
         <input type="text" style={style} onChange={props.changed} value={props.userName} />
     );
